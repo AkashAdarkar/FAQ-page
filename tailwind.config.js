edb7eb40b691fdf46de48c0be09c37359dc9bc43
akashadarkar <aregret01@gmail.com>
2025-01-16 18:47:00 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        // Add more custom font families as needed
+      },
+    },
+  },
+  plugins: [],
+}
+
